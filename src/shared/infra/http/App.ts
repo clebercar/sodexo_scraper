10d3c89561b3c4bf00d4e8fb552dfbc95fbbdfc9
cleber <contatoclebercarvalho@gmail.com>
2020-07-11 01:50:33 -0300

    chore: add bootstrap file

---
+++ src/shared/infra/http/App.ts
@@ -0,0 +1,56 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+import '@shared/container';
+import '@shared/infra/mongoose';
+
+import express, { Express, Request, Response, NextFunction } from 'express';
+import AppError from '@shared/errors/AppError';
+import cors from 'cors';
+
+import routes from './routes';
+
+class App {
+  public readonly express: Express;
+
+  constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+    this.errors();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+    this.express.use(
+      cors({
+        origin: '*',
+        optionsSuccessStatus: 200,
+      }),
+    );
+  }
+
+  errors() {
+    this.express.use(
+      (err: Error, req: Request, res: Response, _next: NextFunction) => {
+        if (err instanceof AppError) {
+          return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+          });
+        }
+
+        return res.status(500).json({
+          status: 'error',
+          message: 'Internal server error.',
+        });
+      },
+    );
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+}
+
+export default new App().express;
