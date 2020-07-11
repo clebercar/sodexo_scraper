a485aeb5be13d423390dc84dd456ff2e0365dd11
cleber <contatoclebercarvalho@gmail.com>
2020-07-11 01:44:10 -0300

    chore(jest): initial coverage config for jest

--- jest.config.js
+++ jest.config.js
@@ -21,9 +21,9 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: [
-  //   '<rootDir>/src/modules/**/services/*.ts'
-  // ],
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/services/*.ts'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
